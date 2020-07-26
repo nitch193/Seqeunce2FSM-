b2756203fa977b8d7bf8746ea09e9de0061e0661
nitch193 <42943921+nitch193@users.noreply.github.com>
2020-07-26 14:01:45 +0530

    Delete FSM.py

--- FSM.py
+++
@@ -1,8 +0,0 @@
-from machines import MooreMachine, MealyMachine
-seq = input("Enter binary input sequnce to detect : ")
-
-mooreFSM = MooreMachine(seq)
-mooreFSM.render_fsm(save_dot=True)
-
-mealyFSM = MealyMachine(seq)
-mealyFSM.render_fsm(save_dot=True)
